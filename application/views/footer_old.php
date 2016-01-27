77adb822108f8d1df4384080fc9a57dcb4e3e9d9
Prasith Lakshan <prasith.lakshan@yahoo.com>
2016-01-27 14:13:32 +0530

    Old header and footer for AceAdmin template. To be removed in the future

---
+++ application/views/footer_old.php
@@ -0,0 +1,40 @@
+
+
+							<!-- PAGE CONTENT ENDS -->
+						</div><!-- /.col -->
+					</div><!-- /.row -->
+				</div><!-- /.page-content -->
+			</div><!-- /.main-content -->
+		</div><!-- /.main-container -->
+
+		<!-- basic scripts -->
+
+
+		<script type="text/javascript">
+			if('ontouchstart' in document.documentElement) document.write("<script src='<?php echo base_url(); ?>assets/js/jquery.mobile.custom.js'>"+"<"+"/script>");
+		</script>
+		<script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
+
+		<!-- DataTables -->
+		<script src="<?php echo base_url(); ?>assets/plugins/datatables/jquery.dataTables.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/plugins/datatables/dataTables.bootstrap.min.js"></script>
+
+		<script src="<?php echo base_url(); ?>assets/js/custom/sidebar-activate.js"></script>-]
+
+		<!-- ace scripts -->
+		<script src="<?php echo base_url(); ?>assets/js/ace/elements.scroller.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/ace/elements.spinner.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/ace/elements.aside.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/ace/ace.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/ace/ace.ajax-content.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/ace/ace.touch-drag.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/ace/ace.sidebar.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/ace/ace.sidebar-scroll-1.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/ace/ace.submenu-hover.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/ace/ace.widget-box.js"></script>
+
+		<!-- inline scripts related to this page -->
+
+		<script type="text/javascript"> ace.vars['base'] = '..'; </script>
+	</body>
+</html>
