c181055150a566144855c99ad07eff0f6bc3a120
Prasith Lakshan <prasith.lakshan@yahoo.com>
2016-01-27 14:35:12 +0530

    Updated DB script

--- spark_hms.sql
+++ spark_hms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 26, 2016 at 11:44 PM
+-- Generation Time: Jan 27, 2016 at 02:33 PM
 -- Server version: 5.5.46-0ubuntu0.14.04.2
 -- PHP Version: 5.5.9-1ubuntu4.14
 
@@ -23,6 +23,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `computer_details`
+--
+
+CREATE TABLE IF NOT EXISTS `computer_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `computer_id` varchar(20) NOT NULL,
+  `location` varchar(20) NOT NULL,
+  `processor` varchar(50) NOT NULL,
+  `motherboard` varchar(50) NOT NULL,
+  `ram` varchar(30) NOT NULL,
+  `hdd` varchar(15) NOT NULL,
+  `monitor` int(11) NOT NULL DEFAULT '0',
+  `mouse` int(11) NOT NULL DEFAULT '0',
+  `keyboard` int(11) NOT NULL DEFAULT '0',
+  `status` varchar(30) NOT NULL,
+  `note` varchar(200) NOT NULL,
+  `created_date` varchar(30) NOT NULL,
+  `created_time` varchar(15) NOT NULL,
+  `created_by` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `computer_id` (`computer_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `computer_details`
+--
+
+INSERT INTO `computer_details` (`id`, `computer_id`, `location`, `processor`, `motherboard`, `ram`, `hdd`, `monitor`, `mouse`, `keyboard`, `status`, `note`, `created_date`, `created_time`, `created_by`) VALUES
+(1, 'WK01', 'LAB01', 'i3', 'Z97', '4GB DDR3', '500GB', 0, 1, 1, 'Functional', '', '2016-01-27', '', 'admin'),
+(3, 'WK02', 'LAB01', 'i3', 'Z97', '4GB DDR3', '500GB', 1, 1, 1, 'Requires Repairs', '', '2016-01-27 12:18:21 am', '', 'admin'),
+(4, 'WK03', 'LAB01', 'i3', 'Z97', '4GB DDR3', '500GB', 1, 1, 0, 'Functional', '', '2016-01-27', '12:21:35 am', 'admin'),
+(5, 'WK04', 'LAB01', 'i3', 'Z97', '4GB DDR3', '500GB', 1, 1, 0, 'Functional', '', '2016-01-27', '12:24:04 am', 'admin'),
+(6, 'WK05', 'LAB01', 'i3', 'Z97', '4GB DDR3', '500GB', 1, 1, 0, 'Out of service', '', '2016-01-27', '12:24:59 am', 'admin'),
+(8, 'WK06', 'LAB02', 'i3(4010) 2.3Ghz ', 'Z97-A', '4GB DDR3', '500GB', 1, 1, 1, 'Functional', '', '2016-01-27', '12:15:41 pm', 'admin');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -35,16 +73,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created_date` date NOT NULL,
   `removed_date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `emp_id`, `username`, `password`, `status`, `created_date`, `removed_date`) VALUES
-(1, '01', 'admin', 'aaa', 'active', '2016-01-20', NULL),
+(1, '01', 'admin', 'sdsds', 'active', '2016-01-20', NULL),
 (5, '121', 'ss', 'sdfsdf', 'active', '2016-01-24', NULL),
-(14, '32', 'test', 'cvcvc', 'active', '2016-01-25', NULL);
+(14, '32', 'test', 'sdfsd', 'active', '2016-01-25', NULL),
+(15, '11', 'qsa', 'asas', 'active', '2016-01-27', NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
