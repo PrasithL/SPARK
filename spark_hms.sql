c8203dd228b0ee3f1819033985659cc28f81e075
Prasith Lakshan <prasith.lakshan@yahoo.com>
2016-01-28 22:29:38 +0530

    Updated DB script. added 'location_history' table

--- spark_hms.sql
+++ spark_hms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 27, 2016 at 02:33 PM
+-- Generation Time: Jan 28, 2016 at 10:28 PM
 -- Server version: 5.5.46-0ubuntu0.14.04.2
 -- PHP Version: 5.5.9-1ubuntu4.14
 
@@ -44,19 +44,46 @@ CREATE TABLE IF NOT EXISTS `computer_details` (
   `created_by` varchar(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `computer_id` (`computer_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `computer_details`
 --
 
 INSERT INTO `computer_details` (`id`, `computer_id`, `location`, `processor`, `motherboard`, `ram`, `hdd`, `monitor`, `mouse`, `keyboard`, `status`, `note`, `created_date`, `created_time`, `created_by`) VALUES
-(1, 'WK01', 'LAB01', 'i3', 'Z97', '4GB DDR3', '500GB', 0, 1, 1, 'Functional', '', '2016-01-27', '', 'admin'),
-(3, 'WK02', 'LAB01', 'i3', 'Z97', '4GB DDR3', '500GB', 1, 1, 1, 'Requires Repairs', '', '2016-01-27 12:18:21 am', '', 'admin'),
-(4, 'WK03', 'LAB01', 'i3', 'Z97', '4GB DDR3', '500GB', 1, 1, 0, 'Functional', '', '2016-01-27', '12:21:35 am', 'admin'),
-(5, 'WK04', 'LAB01', 'i3', 'Z97', '4GB DDR3', '500GB', 1, 1, 0, 'Functional', '', '2016-01-27', '12:24:04 am', 'admin'),
+(1, 'WK01', 'LAB01', 'intel i5 2Ghz', 'Z97', '4GB DDR3', '500GB', 1, 1, 1, 'Functional', '', '2016-01-27', '', 'admin'),
+(3, 'WK02', 'LAB02', 'intel i5 2Ghz', 'Z97', '4GB DDR3', '500GB', 1, 1, 1, 'Requires Repairs', '', '2016-01-27 12:18:21 am', '', 'admin'),
+(4, 'WK03', 'LAB01', 'i3(4010) 2.3Ghz', 'Z97', '4GB DDR3', '500GB', 1, 1, 0, 'Functional', '', '2016-01-27', '12:21:35 am', 'admin'),
+(5, 'WK04', 'LAB01', 'i3(4010) 2.3Ghz', 'Z97', '4GB DDR3', '500GB', 1, 1, 0, 'Functional', '', '2016-01-27', '12:24:04 am', 'admin'),
 (6, 'WK05', 'LAB01', 'i3', 'Z97', '4GB DDR3', '500GB', 1, 1, 0, 'Out of service', '', '2016-01-27', '12:24:59 am', 'admin'),
-(8, 'WK06', 'LAB02', 'i3(4010) 2.3Ghz ', 'Z97-A', '4GB DDR3', '500GB', 1, 1, 1, 'Functional', '', '2016-01-27', '12:15:41 pm', 'admin');
+(8, 'WK06', 'LAB02', 'i3(4010) 2.3Ghz ', 'Z97-A', '4GB DDR3', '500GB', 1, 1, 1, 'Functional', 'fgdfgd11', '2016-01-27', '12:15:41 pm', 'admin'),
+(9, 'WK07', 'LAB09', 'i3(4010) 2.3Ghz', 'Z97-A', '4GB DDR3', '500GB', 1, 1, 1, 'Functional', 'test test test... jkh', '2016-01-28', '04:29:44 pm', 'admin'),
+(10, 'WK08', 'LAB01', 'i3(4010) 2.3Ghz', 'Z97', '4GB DDR3', '500GB', 1, 1, 1, 'Requires Repairs', 'some OS issues', '2016-01-28', '09:51:25 pm', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location_history`
+--
+
+CREATE TABLE IF NOT EXISTS `location_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `computer_id` varchar(20) NOT NULL,
+  `location` varchar(20) NOT NULL,
+  `created_by` varchar(20) NOT NULL,
+  `created_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `location_history`
+--
+
+INSERT INTO `location_history` (`id`, `computer_id`, `location`, `created_by`, `created_date`) VALUES
+(2, 'WK02', 'LAB01', 'admin', '2016-01-28'),
+(3, 'WK02', 'LAB02', 'admin', '2016-01-28'),
+(4, 'WK08', 'LAB02', 'admin', '2016-01-28'),
+(5, 'WK08', 'LAB01', 'admin', '2016-01-28');
 
 -- --------------------------------------------------------
 
