895fafbfd7fb34286d9b8f2de73d220668339a37
Prasith Lakshan <prasith.lakshan@yahoo.com>
2016-01-28 22:24:47 +0530

    enabled error logging in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -207,7 +207,7 @@ $config['directory_trigger'] = 'd';
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 1;
 
 /*
 |--------------------------------------------------------------------------
@@ -218,7 +218,7 @@ $config['log_threshold'] = 0;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '';
+$config['log_path'] = '/var/www/html/SPARK/application/logs/';
 
 /*
 |--------------------------------------------------------------------------
