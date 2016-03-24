adc768dab8b816a87525b350356ebb18f89a048b
Prasith Lakshan <prasith.lakshan@yahoo.com>
2016-03-24 13:06:29 +0530

    The timezone is defined in the header for Date() to use.

--- application/views/header.php
+++ application/views/header.php
@@ -1,4 +1,9 @@
 <!-- @author Prasith Lakshan_IBIT03 -->
+<?php
+    // setting the timezone for the Date() to use
+    // if not set, an error is thrown
+    date_default_timezone_set("UTC")
+?>
 
 <!DOCTYPE html>
 <!--
