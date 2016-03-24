09e5d07824b16bf380fa842c33701d239a458bef
Prasith Lakshan <prasith.lakshan@yahoo.com>
2016-03-24 14:18:17 +0530

    Set the base URL. Not sure if it's correct.

--- application/config/config.php
+++ application/config/config.php
@@ -20,7 +20,7 @@ date_default_timezone_set("UTC");
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/SPARK/';
 
 /*
 |--------------------------------------------------------------------------
@@ -221,7 +221,7 @@ $config['log_threshold'] = 1;
 | application/logs/ directory. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '/var/www/html/SPARK/application/logs/';
+$config['log_path'] = '';
 
 /*
 |--------------------------------------------------------------------------
